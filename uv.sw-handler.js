24af01f917b586e260fe379e9b65ff9571ee71ad
gachalifefangirl12345 <100178320+DarkModeGamersUnblocked@users.noreply.github.com>
2022-05-02 22:15:05 -0400

    Update uv.sw-handler.js

--- uv.sw-handler.js
+++ uv.sw-handler.js
@@ -1,1 +1,1 @@
-importScripts('./uv/uv.sw.js');const sw=new UVServiceWorker();self.addEventListener('fetch',event=>event.respondWith(sw.fetch(event)));
+importScripts('./Ultraviolet-Core-d815552396a9f13e19efc2320a7b8b935ef5fc9c/uv.sw.js');const sw=new UVServiceWorker();self.addEventListener('fetch',event=>event.respondWith(sw.fetch(event)));
